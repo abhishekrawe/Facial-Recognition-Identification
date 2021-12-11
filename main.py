af88fab6d078aeb5ecd58adb1c6db58f68afcd1c
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-12 01:27:32 +0530

    places images at cornor

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ class Face_recognition_System:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
+        
+        img=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\jsx.png")
+        img=img.resize((500,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+
 
 
 

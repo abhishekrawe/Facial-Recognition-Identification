85735d1975e653ac0a32cb7b5ba5185c267596a2
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-12 02:48:26 +0530

    created button

--- main.py
+++ main.py
@@ -41,12 +41,98 @@ class Face_recognition_System:
         f_lbl=Label(self.root,image=self.photoimg2)
         f_lbl.place(x=1000,y=0,width=550,height=130)
 
-        
 
 
 
 
+        #Bg Image
+        img3=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\images.jpg")
+        img3=img3.resize((1530,710),Image.ANTIALIAS)
+        self.photoimg3=ImageTk.PhotoImage(img3)
 
+        bg_img=Label(self.root,image=self.photoimg3)
+        bg_img.place(x=0,y=130,width=1530,height=710)
+
+        title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE" , font=("times new roman",35,"bold"),bg="white",fg="red")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
+
+
+        # student button
+        img4=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img4=img4.resize((220,220),Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img,image=self.photoimg4, cursor="hand2")
+        b1.place(x=200, y=100,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Student Details" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200, y=300,width =220 , height=40)
+
+
+         # Detect face button
+        img5=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img5=img5.resize((220,220),Image.ANTIALIAS)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5, cursor="hand2")
+        b1.place(x=630, y=100,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Face Detector" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=630, y=300,width =220 , height=40)
+
+
+
+        # Attendenc face button
+        img6=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img6=img6.resize((220,220),Image.ANTIALIAS)
+        self.photoimg6=ImageTk.PhotoImage(img6)
+
+        b1=Button(bg_img,image=self.photoimg6, cursor="hand2")
+        b1.place(x=1100, y=100,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Attendance" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100, y=300,width =220 , height=40)
+
+
+        # Train face button
+        img8=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img8=img8.resize((220,220),Image.ANTIALIAS)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+
+        b1=Button(bg_img,image=self.photoimg8, cursor="hand2")
+        b1.place(x=200, y=380,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Train Data" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200, y=580,width =220 , height=40)
+
+        # Train face button
+        img8=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img8=img8.resize((220,220),Image.ANTIALIAS)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+
+        b1=Button(bg_img,image=self.photoimg8, cursor="hand2")
+        b1.place(x=200, y=380,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Train Data" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200, y=580,width =220 , height=40)
+
+
+
+        # Photos face button
+        img9=Image.open(r"C:\Users\HP\Desktop\facerecognisationsystem\college_images\search.png")
+        img9=img9.resize((220,220),Image.ANTIALIAS)
+        self.photoimg9=ImageTk.PhotoImage(img9)
+
+        b1=Button(bg_img,image=self.photoimg9, cursor="hand2")
+        b1.place(x=630, y=380,width =220 , height=220)
+
+        b1_1=Button(bg_img,text="Train Data" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=630, y=580,width =220 , height=40)
+
+
+
+
+      
 
 
 

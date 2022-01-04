24e4168b34da7b672ce4b1b113ffcc532c86d973
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-01-04 17:08:19 +0530

    connect the train data with mainpage : train.py - main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import*
 from tkinter import ttk
 from PIL import Image,ImageTk
-from student import Student
 import os
 from student import Student
 from train import Train
@@ -78,10 +77,10 @@ class Face_recognition_System:
         img5=img5.resize((220,220),Image.ANTIALIAS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
-        b1=Button(bg_img,image=self.photoimg5, cursor="hand2", command=self.face_data)
+        b1=Button(bg_img,image=self.photoimg5, cursor="hand2")
         b1.place(x=630, y=100,width =220 , height=220)
 
-        b1_1=Button(bg_img,text="Face Detector" , cursor="hand2",command=self.face_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="Face Detector" , cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=630, y=300,width =220 , height=40)
 
 
@@ -103,10 +102,10 @@ class Face_recognition_System:
         img8=img8.resize((220,220),Image.ANTIALIAS)
         self.photoimg8=ImageTk.PhotoImage(img8)
 
-        b1=Button(bg_img,image=self.photoimg8, cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2",command=self.train_data)
         b1.place(x=200, y=380,width =220 , height=220)
 
-        b1_1=Button(bg_img,text="Train Data" , cursor="hand2" ,  font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="Train Data",cursor="hand2",command=self.train_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=200, y=580,width =220 , height=40)
 
 
@@ -149,9 +148,9 @@ class Face_recognition_System:
 
 
 
-    def face_data(self):
+    def train_data(self):
         self.new_window=Toplevel(self.root)
-        self.app=Face_Recognition(self.new_window)
+        self.app=Train(self.new_window)
 
 
 

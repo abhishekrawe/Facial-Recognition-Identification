23594f0901c73c383135e9d2094468d78638770a
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-01-04 19:02:42 +0530

    connected attence system and you have matched the data display the name in abhishek.csv file: face_recognisation

--- face_recognition.py
+++ face_recognition.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
+from time import strftime
+from datetime import datetime
 import cv2
 import os
 import numpy as np
@@ -39,6 +41,23 @@ class Face_Recognition:
         b1_1=Button(f_lbl,text="Face Recognition",command=self.face_recog,cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=365, y=620,width =200 , height=40)
 
+    # =========================attendence==============================
+
+    def mark_attendance(self,i,r,n,d):
+        with open("abhishek.csv","r+",newline="\n") as f:
+            myDataList=f.readlines()
+            name_list=[]
+            for line in myDataList:
+                entry=line.split((","))
+                name_list.append(entry[0])
+            if((i not in name_list) and (r not in name_list) and (n not in name_list) and (d not in name_list)):    
+               now=datetime.now()
+               d1=now.strftime("%d/%m/%Y")
+               dtString=now.strftime("%H:%M:%S")
+               f.writelines(f"\n{i},{r},{n},{d},{dtString},{d1},Preset")
+
+
+
     # ======================face recognition ===================
 
     def face_recog(self):
@@ -68,11 +87,16 @@ class Face_Recognition:
                 d=my_cursor.fetchone()
                 d="+".join(d)
 
+                my_cursor.execute("select Student_id from student where Student_id="+str(id))
+                i=my_cursor.fetchone()
+                i="+".join(i)
 
                 if confidence>77:
+                    cv2.putText(img,f"ID:{i}",(x,y-75),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    self.mark_attendance(i,r,n,d)
                 else:
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
                     cv2.putText(img,"Unknown Face",(x,y-5 ),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-draw_boundry
+draw
